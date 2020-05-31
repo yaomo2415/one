42b8862c09d0cc36a02261ea6029708651bf69ca
yaomo <www.yaomo2415@163.com>
2020-05-31 16:44:05 +0800

    login function frontend 50%

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-   <h1>One Short Story</h1>
-    <h3>By O.K.</h3>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    // HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// 导入图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入字体
+import './assets/fonts/font-slogan.css'
 Vue.config.productionTip = false
 
 new Vue({
